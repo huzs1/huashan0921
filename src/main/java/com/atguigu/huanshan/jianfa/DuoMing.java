007581fcdfba2035b0848ef9a1a9ed6e18f2b859
huzs1 <huzhengsong1@163.com>
2021-01-17 00:38:51 +0800

    增加三仙剑

---
+++ src/main/java/com/atguigu/huanshan/jianfa/DuoMing.java
@@ -0,0 +1,16 @@
+package com.atguigu.huanshan.jianfa;
+
+public class DuoMing {
+    public static void main(String[] args) {
+        /*
+关于idea git 文件状态的颜色
+红色  未追踪的文件
+绿色  进入暂存区
+黑色  已提交到本地库中，而且未被修改过
+蓝色  已提交到本地库中，被修改过
+*/
+        System.out.println("夺命连环三仙剑");
+        System.out.println("夺命连环三仙剑");
+        System.out.println("夺命连环三仙剑");
+    }
+}
